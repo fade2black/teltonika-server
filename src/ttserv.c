148f923ee366ab15c800ce4b0bbded1f1e2c5c54
fade2black <bkuliyev@gmail.com>
2016-06-20 14:05:44 +0500

    Bugfix

--- src/ttserv.c
+++ src/ttserv.c
@@ -76,6 +76,7 @@ echo_read_cb(struct bufferevent *bev, void *ctx)
   }
 
   slot = *((int*) g_hash_table_lookup(hash, &bev));
+  printf("slot: %d\ncopying broadcast info...", slot);
   strcpy(bi.ip_address, clients[slot].ip_address);
   strcpy(bi.mes, input_buffer);
 

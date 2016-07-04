2287af4ba1c671dd80935d748618b83dd58d49e7
fade2black <bkuliyev@gmail.com>
2016-07-05 00:22:24 +0500

    Bug fix

--- include/clients_module.h
+++ include/clients_module.h
@@ -14,7 +14,7 @@ typedef struct
 
 void init_clients_hash();
 void add_client(struct bufferevent *bev);
-client_info* client get_client(struct bufferevent *bev);
+client_info* get_client(struct bufferevent *bev);
 void remove_client(struct bufferevent *bev);
 
 #endif
